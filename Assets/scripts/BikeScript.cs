81ba248e04ee66e41399d4730106dff31e2e6cfa
Julian <julian@contingent.dk>
2015-09-19 17:54:34 +0200

    klsd

---
+++ Assets/scripts/BikeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeScript : MonoBehaviour {
+
+	public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += Vector3.forward * speed * Time.fixedDeltaTime;
+	}
+}
---
+++ Assets/scripts/RotateLikeSunScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLikeSunScript : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(transform.position, new Vector3(0.2f, 0.8f, 0f), speed * Time.deltaTime);
+	}
+}
