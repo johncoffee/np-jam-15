c64cbeaf39c1cc9523015720703fc33f3d0a82ab
Julian <julian@contingent.dk>
2015-09-19 21:43:12 +0200

    something works now

--- Assets/scripts/BikeScript.cs
+++ Assets/scripts/BikeScript.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class BikeScript : MonoBehaviour {
 
-	public float speed = 2f;
-
+	public Vector3 speed = new Vector3();
+	public float accelerate = 0.2f;
+	public float leftRight = 0.1f;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +14,21 @@ public class BikeScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.position += Vector3.forward * speed * Time.fixedDeltaTime;
+		transform.position += speed * Time.fixedDeltaTime;
+	}
+
+	void Update() {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.right * -leftRight * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * leftRight * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			speed += Vector3.forward * accelerate * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			speed += Vector3.forward * -accelerate * Time.deltaTime;
+		}
 	}
 }
