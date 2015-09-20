cd3c4edad897088ac171fe00aa77ebb64ef432ac
Julian <julian@contingent.dk>
2015-09-20 22:55:40 +0200

    trees stuff

---
+++ Assets/Random.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Random : MonoBehaviour {
+
+	public float x0, x1, y0, y1, z0, z1;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
